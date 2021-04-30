387c912d35ec8348c1852363079770a2acc2974c
Abdul Latif <alatif.wijaya@gmail.com>
2021-04-30 16:31:28 +0700

    first commit

---
+++ Jawaban.js
@@ -0,0 +1,19 @@
+function pairedSocks( n, arr ) {
+    let sortedArr = arr.sort( (a,b) => a - b);
+    let pairs = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        if ( sortedArr[i] === sortedArr[i + 1]) {
+            pairs++;
+            i += 1;
+        }
+    }
+
+    return pairs;
+}
+
+let a = 9
+let socks = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+
+console.log("Jawaban: ", pairedSocks(a,socks));
